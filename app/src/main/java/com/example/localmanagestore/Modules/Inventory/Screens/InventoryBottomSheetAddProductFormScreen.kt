1d742cce29c3837768316fdf89b7737fa888f976
Gerardo SC <gerardo.sc.dev@gmail.com>
2024-07-31 12:41:51 -0600

    save photo to gallery

--- app/src/main/java/com/example/localmanagestore/Modules/Inventory/Screens/InventoryBottomSheetAddProductFormScreen.kt
+++ app/src/main/java/com/example/localmanagestore/Modules/Inventory/Screens/InventoryBottomSheetAddProductFormScreen.kt
@@ -1,7 +1,12 @@
 package com.example.localmanagestore.Modules.Inventory.Screens
 
 import android.Manifest
+import android.content.ContentValues
+import android.content.Context
 import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.launch
@@ -62,11 +67,16 @@ import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import java.io.File
+import java.io.File.separator
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
 fun InventoryBottomSheetAddProductForm(onDismiss: () -> Unit) {
 
+    val currentContext = LocalContext.current
     val viewModel = InventoryBottomSheetAddProductFormViewModel(context = LocalContext.current)
 
     val (barcodeValue, onBarcodeValueChange) = rememberSaveable { mutableStateOf("") }
@@ -105,6 +115,10 @@ fun InventoryBottomSheetAddProductForm(onDismiss: () -> Unit) {
         contract = ActivityResultContracts.TakePicturePreview()
     ) {
         bitmapProduct.value = it
+        it?.let {
+            saveImage(it, currentContext)
+        }
+
     }
 
     if (isValidatePermissionState) {
@@ -300,6 +314,58 @@ fun ShowPermission(cameraPermissionState: PermissionState) {
             // Show rationale if needed
         } else {
             requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
+    }
+}
+
+/// @param folderName can be your app's name
+private fun saveImage(bitmap: Bitmap, context: Context) {
+    if (android.os.Build.VERSION.SDK_INT >= 29) {
+        val values = contentValues()
+        values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/" + "ProductsImage")
+        values.put(MediaStore.Images.Media.IS_PENDING, true)
+        // RELATIVE_PATH and IS_PENDING are introduced in API 29.
+
+        val uri: Uri? = context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        if (uri != null) {
+            saveImageToStream(bitmap, context.contentResolver.openOutputStream(uri))
+            values.put(MediaStore.Images.Media.IS_PENDING, false)
+            context.contentResolver.update(uri, values, null, null)
+        }
+    } else {
+        val directory = File(Environment.getExternalStorageDirectory().toString() + separator + "ProductsImage")
+        // getExternalStorageDirectory is deprecated in API 29
+
+        if (!directory.exists()) {
+            directory.mkdirs()
+        }
+        val fileName = System.currentTimeMillis().toString() + ".png"
+        val file = File(directory, fileName)
+        saveImageToStream(bitmap, FileOutputStream(file))
+        if (file.absolutePath != null) {
+            val values = contentValues()
+            values.put(MediaStore.Images.Media.DATA, file.absolutePath)
+            // .DATA is deprecated in API 29
+            context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        }
+    }
+}
+
+private fun contentValues() : ContentValues {
+    val values = ContentValues()
+    values.put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+    values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000);
+    values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+    return values
+}
+
+private fun saveImageToStream(bitmap: Bitmap, outputStream: OutputStream?) {
+    if (outputStream != null) {
+        try {
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+            outputStream.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
