615f2550535bfc095d73d47da61b29ac20728ff2
Gerardo SC <gerardo.sc.dev@gmail.com>
2024-08-09 11:01:31 -0600

    add splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("io.coil-kt:coil-compose:2.7.0")
     implementation("androidx.activity:activity-ktx:1.9.0")
--- app/src/main/java/com/example/localmanagestore/MainActivity.kt
+++ app/src/main/java/com/example/localmanagestore/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Scaffold
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.localmanagestore.CommonUtils.AppComponents.AppButtonNavBar.AppButtonNavBar
 import com.example.localmanagestore.Modules.Inventory.Screens.InventoryScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             Scaffold (
 //                bottomBar = { AppButtonNavBar() }
